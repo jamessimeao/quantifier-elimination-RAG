56843f3d8f68ea6e4230965f1d1bc24bb07e1a71
james <jamessimeao@outlook.com>
2025-12-22 16:55:02 -0300

    Add test for XSRemS

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "../include/srs.hpp"
+#include "../include/xsrs.hpp"
 
 void test_SRemS()
 {
@@ -45,8 +46,51 @@ void test_SRemS()
     std::cout << "Finished." << std::endl;
 }
 
+void test_XSRemS()
+{
+    CaCtxXX ctxXX;
+
+    CaXX one = CaXX(ctxXX);
+    CaXX two = CaXX(ctxXX);
+    CaXX minus_one = CaXX(ctxXX);
+
+    one.set_si(1);
+    two.set_si(2);
+    minus_one.set_si(-1);
+    
+    CaPolyXX P = CaPolyXX(ctxXX);
+    CaPolyXX Q = CaPolyXX(ctxXX);
+    P.set_name("P");
+    Q.set_name("Q");
+    
+    // P = X^2 +2X +1
+    P.set_coeff_ca(0, one);
+    P.set_coeff_ca(1, two);
+    P.set_coeff_ca(2, one);
+
+    // Q = X^2 -1
+    Q.set_coeff_ca(0, minus_one);
+    Q.set_coeff_ca(2, one);
+
+    // print P and Q
+    P.println();
+    Q.println();
+
+    std::cout << "Computing SRemS" << std::endl;
+
+    // Extended signed remainder sequence
+    XSRemS srs = XSRemS(P, Q, ctxXX);
+
+    std::cout << "Print the gcd" << std::endl;
+
+    CaPolyXX & gcd = srs.gcd();
+    gcd.println();
+
+    std::cout << "Finished." << std::endl;
+}
+
 int main()
 {
-    test_SRemS();
+    test_XSRemS();
     return 0;
 }
