59b772b666ba33151d116315b3e245261429133f
james <jamessimeao@outlook.com>
2025-12-21 23:25:11 -0300

    Make a class SRemS that computes and stores the signed remainder sequence of two polynomials.

--- include/signed-remainder.hpp
+++ include/signed-remainder.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
 #include <flint/ca_poly.h>
+#include "poly.hpp"
+#include <vector>
 
-// Computes the signed remainder sequence for P and Q and stores it in SRemS
-int compute_signed_remainder_sequence(ca_poly_t * SRemS, ca_poly_t P, ca_poly_t Q, ca_ctx_t ctx);
+class SRemS
+{
+private:
+    std::vector<PolyXX*> sequence;
+    size_t last_index;
+public:
+    SRemS(PolyXX & P, PolyXX & Q, CtxXX & ctxXX);
+    ~SRemS();
+    size_t get_last_index();
+    PolyXX & gcd();
+};
--- src/signed-remainder.cpp
+++ src/signed-remainder.cpp
@@ -1,85 +1,100 @@
 #include "../include/signed-remainder.hpp"
 
-// It assumes that SRemS is an array big enough to store the signed remainder sequence.
-// Code must be changed to allocate memory as needed.
-int compute_signed_remainder_sequence(ca_poly_t * SRemS, ca_poly_t P, ca_poly_t Q, ca_ctx_t ctx)
+SRemS::SRemS(PolyXX & P, PolyXX & Q, CtxXX & ctxXX)
 {
-    truth_t P_is_zero = ca_poly_check_is_zero(P, ctx);
-    truth_t Q_is_zero = ca_poly_check_is_zero(Q, ctx);
+    truth_t P_is_zero = P.check_is_zero();
+    truth_t Q_is_zero = Q.check_is_zero();
 
     if(P_is_zero == T_UNKNOWN || Q_is_zero == T_UNKNOWN)
     {
-        printf("Error in compute_signed_remainder_sequence: couldn't check if P or Q is 0.");fflush(stdout);
-        return -1;
+        throw std::runtime_error("Error in compute_signed_remainder_sequence: couldn't check if P or Q is 0.\n");
     }
 
     if(P_is_zero == T_TRUE && Q_is_zero == T_TRUE)
     {
-        printf("Error in compute_signed_remainder_sequence: both P and Q are 0.");fflush(stdout);
-        return -1;
+        throw std::runtime_error("Error in compute_signed_remainder_sequence: both P and Q are 0.\n");
     }
 
     // Here we know if P and Q are 0 or not, and that not both are 0.
 
-    // Empty SRemS
-    //ca_poly_vec_set_length(SRemS,0,ctx); ca_poly_vec is incomplete
+    // Empty the signed remainder sequence
+    sequence.clear();
 
     // Add the first polynomial of SRemS, which is P
-    //ca_poly_vec_append(SRemS, P, ctx); ca_poly_vec is incomplete
-    ca_poly_set(SRemS[0], P, ctx);
+    std::cout << "push_back" << std::endl;
+    PolyXX * P_copy_ptr = new PolyXX(P);
+    P_copy_ptr->set_name("SRemS_" + std::to_string(0));
+    sequence.push_back(P_copy_ptr);
 
     if(Q_is_zero == T_TRUE)
     {
         // return index of last non zero polynomial of the sequence
-        return 0;
+        last_index = 0;
+        return;
     }
 
     // Add the second polynomial of SRemS, which is Q
-    //ca_poly_vec_append(SRemS, Q, ctx); ca_poly_vec is incomplete
-    ca_poly_set(SRemS[1], Q, ctx);
+    std::cout << "push_back" << std::endl;
+    PolyXX * Q_copy_ptr = new PolyXX(Q);
+    Q_copy_ptr->set_name("SRemS_" + std::to_string(1));
+    sequence.push_back(Q_copy_ptr);
 
     // Recursively add non zero signed remainders to SRemS.
     // Use a variable k to store the index of the last non zero polynomial in SRemS.
     // It will be returned by this function if there is no error.
-    int k = 1;
-    ca_poly_t signed_remainder;
-    ca_poly_init(signed_remainder, ctx);
-    while(1)
+    last_index = 1;
+
+    PolyXX signed_remainder = PolyXX(ctxXX);
+    signed_remainder.set_name("signed_remainder");
+    std::cout << "Entering while loop" << std::endl;
+    while(true)
     {
-        int success = ca_poly_rem(signed_remainder, SRemS[k-1], SRemS[k], ctx);
+        std::cout << "Computing remainder" << std::endl;
+        bool success = signed_remainder.set_to_rem(*sequence[last_index-1], *sequence[last_index]);
         if(!success)
         {
-            printf("Error in compute_signed_remainder_sequence: failed to compute remainder.");fflush(stdout);
-            k = -1;
-            goto end;
+            throw std::runtime_error("Error in compute_signed_remainder_sequence: failed to compute remainder.\n");
         }
 
-        truth_t zero_remainder = ca_poly_check_is_zero(signed_remainder, ctx);
+        truth_t zero_remainder = signed_remainder.check_is_zero();
+        std::cout << "Switch case" << std::endl;
+        // multiply the remainder by -1
+        signed_remainder.set_to_neg();
+        PolyXX * signed_remainder_copy_ptr = new PolyXX(signed_remainder);
         switch(zero_remainder)
         {
             case(T_UNKNOWN):
-                printf("Error in compute_signed_remainder_sequence: failed to check if remainder is zero.");fflush(stdout);
-                ca_poly_clear(signed_remainder, ctx);
-                k = -1;
-                goto end;
+                throw std::runtime_error("Error in compute_signed_remainder_sequence: failed to check if remainder is zero.\n");
 
             case(T_FALSE):
-                //ca_poly_vec_append(SRemS, remainder, ctx); ca_poly_vec is incomplete
-                k++;
-                // multiply the remainder by -1
-                ca_poly_neg(signed_remainder, signed_remainder, ctx);
+                last_index++;
                 // add the signed remainder to SRemS
-                ca_poly_set(SRemS[k], signed_remainder, ctx);
+                signed_remainder_copy_ptr->set_name("SRemS_" + std::to_string(last_index));
+                sequence.push_back(signed_remainder_copy_ptr);
                 break;
 
             case(T_TRUE):
-                goto end;
+                return;
         }
     }
-    end:
-    ca_poly_clear(signed_remainder, ctx);
-    // Return k.
-    // If there was no error, k is the index of the last non zero polynomial in SRemS.
-    // Otherwise, k = -1, which means there was an error.
-    return k;
+}
+
+SRemS::~SRemS()
+{
+    std::cout << "Destructing SRemS...\n";
+    for(PolyXX * poly_ptr : sequence)
+    {
+        poly_ptr->~PolyXX();
+    }
+    std::cout << "Finished destructing SRemS\n";
+}
+
+size_t SRemS::get_last_index()
+{
+    return last_index;
+}
+
+PolyXX & SRemS::gcd()
+{
+    return *sequence[last_index];
 }
