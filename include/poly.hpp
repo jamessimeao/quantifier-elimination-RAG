6f6054ea2082d3847256a0f74e95f04a779837ab
james <jamessimeao@outlook.com>
2025-12-21 23:24:02 -0300

    Make more wrapper methods for ca_poly.h functions and add a name property to PolyXX.

--- include/poly.hpp
+++ include/poly.hpp
@@ -11,9 +11,15 @@ private:
     ca_poly_t poly;
     CtxXX * ctxXX_ptr;
     ca_ctx_t * ctx_ptr();
+    std::string name;
 public:
     PolyXX(CtxXX & ctxXX);
+    PolyXX(PolyXX & P);
     ~PolyXX();
+    void set_name(std::string_view new_name);
     void println();
     void set_coeff(slong n, CaXX& x);
+    void set_to_neg();
+    bool set_to_rem(PolyXX & A, PolyXX & B);
+    truth_t check_is_zero();
 };
--- src/poly.cpp
+++ src/poly.cpp
@@ -2,19 +2,32 @@
 
 PolyXX::PolyXX(CtxXX & ctxXX)
 {
-    printf("Initializing PolyXX...\n");fflush(stdout);
+    std::cout << "Initializing PolyXX...\n";
     ca_poly_init(poly, *ctxXX._ctx_ptr());
     ctxXX_ptr = &ctxXX;
+}
+
+PolyXX::PolyXX(PolyXX & P)
+{
+    ctxXX_ptr = P.ctxXX_ptr;
+    ca_poly_set(poly, P.poly, *ctx_ptr());
 }
     
 PolyXX::~PolyXX()
 {
-    printf("Destructing PolyXX...\n");fflush(stdout);
+    std::cout << "Destructing PolyXX "<< name <<"...\n";
+    println();
     ca_poly_clear(poly, *ctx_ptr());
+}
+
+void PolyXX::set_name(std::string_view new_name)
+{
+    name = new_name;
 }
 
 void PolyXX::println()
 {
+    std::cout << name << ":\n";
     ca_poly_print(poly, *ctx_ptr());
 }
 
@@ -26,4 +39,20 @@ ca_ctx_t * PolyXX::ctx_ptr()
 void PolyXX::set_coeff(slong n, CaXX & x)
 {
     ca_poly_set_coeff_ca(poly,n,*x._ca_ptr(),*ctx_ptr());
+}
+
+void PolyXX::set_to_neg()
+{
+    ca_poly_neg(poly,poly,*ctx_ptr());
+}
+
+bool PolyXX::set_to_rem(PolyXX & A, PolyXX & B)
+{
+    int success = ca_poly_rem(poly, A.poly, B.poly, *ctx_ptr());
+    return success == 1;
+}
+
+truth_t PolyXX::check_is_zero()
+{
+    return ca_poly_check_is_zero(poly, *ctx_ptr());
 }
